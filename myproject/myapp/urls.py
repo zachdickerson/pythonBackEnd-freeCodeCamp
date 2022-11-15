436bfb295783236b1b2de405d47f66e0f1dc1a67
zdickerson <zachary-dickerson@uiowa.edu>
2022-11-15 16:37:51 -0600

    Completion of free code camp tutorials

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.Login, name='login'),
+    path('logout', views.Logout, name='logout'),
+    path('posts/<str:pk>', views.posts, name='posts')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,38 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Fast'
-    feature1.is_true = True
-    feature1.details = 'Our service is very quick'
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Reliable'
-    feature2.is_true = True
-    feature2.details = 'Our service is very reliable'
-
-    feature3 = Feature()
-    feature3.id = 2
-    feature3.name = 'Easy to use'
-    feature3.is_true = False
-    feature3.details = 'Our service is very easy to use'
-
-    feature4 = Feature()
-    feature4.id = 3
-    feature4.name = 'Affordable'
-    feature4.is_true = True
-    feature4.details = 'Our service is very affordable'
-
-    features = [feature1, feature2, feature3, feature4, ]
-
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Already used')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email,password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password Not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect ('login')
+    
+    else:
+        return render(request, 'login.html')
+
+def Logout(request):
+    auth.logout(request)
+    return redirect('/')
+
 def counter(request):
-    text = request.POST['text']
-    amount_of_words = len(text.split())
-    return render(request, 'counter.html', {'amount': amount_of_words})
+    post_s = [1, 2, 3, 4, 5, 'tim', 'tom', 'john']
+    return render(request, 'counter.html', {'post_s': post_s})
+
+def posts(request, pk):
+    return render(request, 'posts.html', {'pk': pk})
